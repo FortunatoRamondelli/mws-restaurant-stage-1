1859fc7ce831e36396be60b72a52807ba1053c67
Fortunato <fortunato.ramondeli@gmail.com>
2018-03-25 21:37:51 +0200

    feat: responsive images

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 300,
+            rename: false,
+            suffix: '_small',
+            quality: 30
+          },
+          {
+            width: 600,
+            rename: false,
+            suffix: '_medium',
+            quality: 30
+          },
+          {
+            width: 800,
+            rename: false,
+            quality: 30
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['img'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['img_src/fixed/*.{gif,jpg,png}'],
+          dest: 'img/',
+          flatten: true,
+        }]
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
