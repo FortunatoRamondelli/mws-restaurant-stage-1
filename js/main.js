00288b2371f96b4d4315b3ad2f661cd1cff2dd81
Fortunato <fortunato.ramondeli@gmail.com>
2018-03-26 21:43:55 +0200

    feat: add service worker

--- js/main.js
+++ js/main.js
@@ -198,3 +198,11 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js').then(function(registration) {
+    console.log("Service Worker registration worked: ", registration);
+  }).catch(function(error) {
+    console.error("Service Worker registration failed: ", error);
+  });
+}
