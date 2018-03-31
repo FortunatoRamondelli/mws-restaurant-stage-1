b377db4206232665ed84824a6ec32cf7ae9194ca
Fortunato <fortunato.ramondeli@gmail.com>
2018-03-31 15:54:05 +0200

    feat: change service worker

--- js/main.js
+++ js/main.js
@@ -199,10 +199,14 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 }
 
+// Check that service workers are registered
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../sw.js').then(function(registration) {
-    console.log("Service Worker registration worked: ", registration);
-  }).catch(function(error) {
-    console.error("Service Worker registration failed: ", error);
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../sw.js').then(function(registration) {
+      console.log("Service Worker registration worked: ", registration);
+    }).catch(function(error) {
+      console.error("Service Worker registration failed: ", error);
+    });
   });
 }
--- sw.js
+++ sw.js
@@ -1,35 +1,40 @@
-const cacheName = "mws-restaurant-stage-1";
-
-const cssFiles = ['styles'];
-const dataFiles = ['restaurants'];
-const imgFiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const jsFiles = ['dbhelper', 'main', 'restaurant_info']
-const htmlFiles = ['index', 'restaurant'];
+const staticCacheName = "mws-restaurant-stage-1v1";
 
 self.addEventListener('install', function(event) {
+  const css  = ['styles'];
+  const data = ['restaurants'];
+  const img  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const js   = ['dbhelper', 'main', 'restaurant_info']
+  const html = ['index', 'restaurant'];
+
+  const urlsToCache = [...css.map (fileName => `/css/${fileName}.css`),
+                       ...data.map(fileName => `/data/${fileName}.json`),
+                       ...img.map (fileName => `/img/${fileName}.jpg`),
+                       ...js.map  (fileName => `/js/${fileName}.js`),
+                       ...html.map(fileName => `/${fileName}.html`)];
+
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll([
-        ...htmlFiles.map( fileName => `/${fileName}.html`),
-        ...cssFiles.map(  fileName => `/css/${fileName}.css`),
-        ...dataFiles.map( fileName => `/data/${fileName}.json`),
-        ...imgFiles.map(  fileName => `/img/${fileName}.jpg`),
-        ...jsFiles.map(   fileName => `/js/${fileName}.js`)
-      ]);
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
+    caches.open(staticCacheName).then(function(cache) {
+      return caches.match(event.request).then(function(response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
           return response;
         }).catch(function(error) {
           console.log(error);
-          return new Response("No internet connection!");
+          return new Response(`<div class="alertbar"
+                                    aria-live="assertive">
+                                 <h1>No internet connection!</h1>
+                               </div>`, {
+                     headers: {'Content-Type': 'text/html'}
+                   });
         });
       });
     })
@@ -39,7 +44,10 @@ self.addEventListener('fetch', function(event) {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promise.all(cacheNames.filter(name => name != cacheName).map(cache => caches.delete(cache)));
+      return Promise.all(
+        cacheNames.filter(name => name.startsWith('mws-')
+                               && name != staticCacheName)
+        .map(cache => caches.delete(cache)));
     })
   );
 });
